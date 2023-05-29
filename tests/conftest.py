4725a7a6aa51629adbfa76437ae0677f85923b45
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2023-05-29 12:31:53 +0200

    add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ from cookiecutter.main import cookiecutter
 from kedro import __version__ as kedro_version
 from kedro.framework.cli.starters import TEMPLATE_PATH
 
-_FAKE_PROJECT_NAME = "fake_project"
+_FAKE_PACKAGE_NAME = "fake_project"
+_FAKE_REPO_NAME = _FAKE_PACKAGE_NAME.replace("_", "-")
 
 
 @pytest.fixture
@@ -14,10 +15,9 @@ def kedro_project(tmp_path):
     config = {
         "output_dir": tmp_path,
         "kedro_version": kedro_version,
-        "project_name": _FAKE_PROJECT_NAME.replace("_", " "),
-        "repo_name": _FAKE_PROJECT_NAME.replace("_", "-"),
-        "python_package": _FAKE_PROJECT_NAME,
-        "include_example": True,
+        "project_name": _FAKE_PACKAGE_NAME.replace("_", " "),
+        "repo_name": _FAKE_REPO_NAME.replace("_", "-"),
+        "python_package": _FAKE_PACKAGE_NAME,
     }
 
     cookiecutter(
@@ -28,33 +28,33 @@ def kedro_project(tmp_path):
     )
 
     shutil.rmtree(
-        tmp_path / "fake-project" / "src" / "tests"
+        config["output_dir"] / _FAKE_REPO_NAME / "src" / "tests"
     )  # avoid conflicts with pytest
 
-    return tmp_path / "fake-project"
+    return tmp_path / _FAKE_REPO_NAME
 
 
 @pytest.fixture
 def kedro_project_iris(tmp_path):
     # TODO : this is also an integration test since this depends from the kedro version
+
     config = {
         "output_dir": tmp_path,
         "kedro_version": kedro_version,
-        "project_name": _FAKE_PROJECT_NAME.replace("_", " "),
-        "repo_name": _FAKE_PROJECT_NAME.replace("_", "-"),
-        "python_package": _FAKE_PROJECT_NAME,
-        "include_example": True,
+        "project_name": _FAKE_PACKAGE_NAME.replace("_", " "),
+        "repo_name": _FAKE_REPO_NAME.replace("_", "-"),
+        "python_package": _FAKE_PACKAGE_NAME,
     }
-
     cookiecutter(
-        str(TEMPLATE_PATH),
+        template="git+https://github.com/kedro-org/kedro-starters.git",
+        no_input=True,
         output_dir=config["output_dir"],
-        no_input=False,
+        directory="pandas-iris",
         extra_context=config,
     )
 
     shutil.rmtree(
-        tmp_path / "fake-project" / "src" / "tests"
+        config["output_dir"] / _FAKE_REPO_NAME / "src" / "tests"
     )  # avoid conflicts with pytest
 
     return tmp_path / "fake-project"
