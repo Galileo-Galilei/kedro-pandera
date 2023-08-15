38027daa9791282857ca1e9a6f62d4125743f733
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2023-08-15 14:35:44 +0200

    :sparkles: Ensure CLI inferred schema is properly parsed in the catalog

--- tests/framework/cli/test_cli_infer_schema.py
+++ tests/framework/cli/test_cli_infer_schema.py
@@ -1,7 +1,7 @@
 import pytest
 from click.testing import CliRunner
 
-from kedro_pandera.framework.cli.cli import infer_schema as cli_infer_schema
+from kedro_pandera.framework.cli.cli import infer_dataset_schema
 
 
 @pytest.mark.parametrize("flag_extension", ["--python", "--yaml"])
@@ -12,11 +12,15 @@ def test_schema_is_persisted_with_correct_extension(
     # launch the command to initialize the project
     cli_runner = CliRunner()
     result = cli_runner.invoke(
-        cli_infer_schema, ["-d", "example_iris_data", flag_extension]
+        infer_dataset_schema,
+        ["-d", "example_iris_data", flag_extension],
     )
     file_extension = "py" if flag_extension == "--python" else "yml"
 
     assert result.exit_code == 0
     assert (
-        kedro_project_iris / "conf" / "local" / f"example_iris_data.{file_extension}"
+        kedro_project_iris
+        / "conf"
+        / "base"
+        / f"catalog_example_iris_data.{file_extension}"
     ).is_file()
