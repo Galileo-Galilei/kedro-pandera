38857bfadb33be2b2bc5b69d61082126adf5d561
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2024-05-06 22:47:04 +0200

    attempt to fix sphinx-docsearch

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     extras_require={
         "doc": [
             "sphinx>=4.5.0,<7.3.0",  # https://github.com/kai687/sphinxawesome-theme/issues/1464
+            "sphinx-docsearch",
             "sphinxawesome-theme",
             "sphinx-markdown-tables~=0.0.15",
             "sphinx-click>=3.1,<6.0",
