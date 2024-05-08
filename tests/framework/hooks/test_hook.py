1d0b43e0de791497b5c89c125a2f603b6e21edf4
Manuel Spierenburg <manuel.spierenburg@sbb.ch>
2024-05-08 16:38:48 +0200

    add feature to convert dataframes with schema for input and output data

--- tests/framework/hooks/test_hook.py
+++ tests/framework/hooks/test_hook.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from kedro.io import DataCatalog
 from kedro.pipeline import node
 from kedro_datasets.pandas import CSVDataset
@@ -6,13 +7,13 @@ from pandera.io import from_yaml
 from kedro_pandera.framework.hooks.pandera_hook import PanderaHook
 
 
-def test_hook():
-    test_schema = from_yaml("tests/data/iris_schema.yml")
+def _run_hook(csv_file, schema_file, convert=False):
+    test_schema = from_yaml(schema_file)
     test_catalog = DataCatalog(
         {
             "iris": CSVDataset(
-                filepath="tests/data/iris.csv",
-                metadata={"pandera": {"schema": test_schema}},
+                filepath=csv_file,
+                metadata={"pandera": {"schema": test_schema, "convert": convert}},
             )
         }
     )
@@ -21,10 +22,35 @@ def test_hook():
     test_node = node(
         name="test_node", func=lambda iris: True, inputs=["iris"], outputs=None
     )
-    test_hook.before_node_run(
+    converted_inputs = test_hook.before_node_run(
         node=test_node,
         catalog=test_catalog,
         inputs=test_inputs,
         is_async=False,
         session_id=0,
+    )
+    return test_inputs, converted_inputs
+
+
+def test_hook():
+    _run_hook(
+        csv_file="tests/data/iris.csv",
+        schema_file="tests/data/iris_schema.yml",
+        convert=False,
+    )
+
+
+def test_hook_with_converting():
+    test_inputs, converted_inputs = _run_hook(
+        csv_file="tests/data/iris.csv",
+        schema_file="tests/data/iris_schema.yml",
+        convert=True,
+    )
+    # test if species column is converted to string type
+    assert ~isinstance(
+        test_inputs["iris"].dtypes["species"], pd.core.arrays.string_.StringDtype
+    )
+    assert isinstance(
+        converted_inputs["iris"].dtypes["species"],
+        pd.core.arrays.string_.StringDtype,
     )
